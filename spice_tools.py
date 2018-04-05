5e97bf6c3de4881d85eaf5bc5349225a6774852b
Nathan Barnes <npbarnes@alaska.edu>
2018-04-05 10:58:33 -0800

    Revert timing.py

--- spice_tools.py
+++ spice_tools.py
@@ -34,9 +34,23 @@ sp.furnsh('./NH_SPICE_KERNELS/data/ik/nh_swap_v200.ti')
 
 ############################################################################
 
+# Define some times in ET that may be useful
+rehersal_start = sp.str2et('2015-07-12T12:00:00.0000')
+rehersal_end   = sp.str2et('2015-07-12T17:00:00.0000')
+
+#flyby_start = sp.str2et('2015-07-14T10:00:00.0000')
+#flyby_end   = sp.str2et('2015-07-14T19:00:00.0000')
+flyby_start = sp.str2et('2015-07-14T11:00:00.0000')
+flyby_end   = sp.str2et('2015-07-14T16:00:00.0000')
+
+close_start = sp.str2et('2015-07-14T11:10:00.0000')
+close_end   = sp.str2et('2015-07-14T14:20:00.0000')
+
+stupid_close_start = sp.str2et('2015-07-14T13:02:00.0000')
+stupid_close_end   = sp.str2et('2015-07-14T13:10:00.0000')
 
 def NH_J2000(et):
-    position, _ = sp.spkpos('New Horizons', et, 'J2000', 'NONE', 'Pluto')
+    position, _ = sp.spkpos('New Horizons', et, 'Pluto', 'NONE', 'Pluto')
     return position
 
 if __name__ == '__main__':
