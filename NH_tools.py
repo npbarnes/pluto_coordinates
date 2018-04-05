f4ebf5eb7aa0d2656a420ae8842782799a13c1af
Nathan Barnes <npbarnes@alaska.edu>
2018-04-05 10:26:45 -0800

    Move timing constants to a separate file

--- NH_tools.py
+++ NH_tools.py
@@ -4,24 +4,10 @@ from astropy.io import fits
 import spiceypy as sp
 import SortedSearch as ss
 from matplotlib.ticker import FuncFormatter
+from timing import *
 
 # Leap Seconds Kernel
 sp.furnsh('/home/nathan/lib/lsk.tls')
-
-# Useful stuff
-rehersal_start = sp.str2et('2015-07-12T12:00:00.0000')
-rehersal_end   = sp.str2et('2015-07-12T17:00:00.0000')
-
-#flyby_start = sp.str2et('2015-07-14T10:00:00.0000')
-#flyby_end   = sp.str2et('2015-07-14T19:00:00.0000')
-flyby_start = sp.str2et('2015-07-14T11:00:00.0000')
-flyby_end   = sp.str2et('2015-07-14T16:00:00.0000')
-
-close_start = sp.str2et('2015-07-14T11:10:00.0000')
-close_end   = sp.str2et('2015-07-14T14:20:00.0000')
-
-stupid_close_start = sp.str2et('2015-07-14T13:02:00.0000')
-stupid_close_end   = sp.str2et('2015-07-14T13:10:00.0000')
 
 @FuncFormatter
 def et_formatter(tickval, tickpos):
